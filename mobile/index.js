43d312c5c36f61b95bf5f63a8cf29f6dfd4b4c06
Kyunghoon <kyunghoon@gmail.com>
2019-08-09 03:21:25 +0900

    added react-native-screens

--- mobile/index.js
+++ mobile/index.js
@@ -2,5 +2,8 @@
 
 import { AppRegistry } from 'react-native';
 import { App } from './src';
+import { useScreens } from 'react-native-screens';
+
+useScreens();
 
 AppRegistry.registerComponent('mobile', () => App);
