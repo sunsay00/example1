c116b1ae6a659a2968fadd04ebc6fa0c502f9015
Kyunghoon <kyunghoon@gmail.com>
2019-08-09 03:14:15 +0900

    added scrollview

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -21,25 +21,27 @@ const Tab1 = () => {
     headerRight: <UI.View style={{ paddingHorizontal: 16 }}><UI.Text weight="bold">Right</UI.Text></UI.View>,
   });
   return (
-    <UI.View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <StatusBar barStyle='dark-content' />
-      <UI.SafeAreaView>
-        <UI.Accent>123456789</UI.Accent>
-        <UI.Header1>Infinage Styleguide</UI.Header1>
-        <UI.Header2>Table of Contents</UI.Header2>
-        <UI.Header2>Components</UI.Header2>
-        <UI.Header4>Accent</UI.Header4>
-        <UI.Header2>123456789</UI.Header2>
-        <UI.Text weight="black" numberOfLines={1}>123456789</UI.Text>
-        <UI.Text weight="bold" numberOfLines={1}>123456789</UI.Text>
-        <UI.Text weight="medium" numberOfLines={1}>123456789</UI.Text>
-        <UI.Text weight="light" numberOfLines={1}>123456789</UI.Text>
-        <UI.Text weight="thin" numberOfLines={1}>123456789</UI.Text>
-        <UI.View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <UI.Button onPress={nav.openDrawer}>Open Drawer</UI.Button>
-        </UI.View>
-      </UI.SafeAreaView>
-    </UI.View>
+    <UI.ScrollView>
+      <UI.View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar barStyle='dark-content' />
+        <UI.SafeAreaView>
+          <UI.Accent>123456789</UI.Accent>
+          <UI.Header1>Infinage Styleguide</UI.Header1>
+          <UI.Header2>Table of Contents</UI.Header2>
+          <UI.Header2>Components</UI.Header2>
+          <UI.Header4>Accent</UI.Header4>
+          <UI.Header2>123456789</UI.Header2>
+          <UI.Text weight="black" numberOfLines={1}>123456789</UI.Text>
+          <UI.Text weight="bold" numberOfLines={1}>123456789</UI.Text>
+          <UI.Text weight="medium" numberOfLines={1}>123456789</UI.Text>
+          <UI.Text weight="light" numberOfLines={1}>123456789</UI.Text>
+          <UI.Text weight="thin" numberOfLines={1}>123456789</UI.Text>
+          <UI.View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <UI.Button onPress={nav.openDrawer}>Open Drawer</UI.Button>
+          </UI.View>
+        </UI.SafeAreaView>
+      </UI.View>
+    </UI.ScrollView>
   );
 }
 
